cbb7b65befa1db932ba39565c24ee7e985dc2cfa
Rodrigo Alba <rodrigoqalba@gmail.com>
2021-11-22 13:21:51 -0400

    commit tree f63e10782404c7d023bbbf47f583d6274d1987f8

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+# def my_method (param)
+#    puts "Running my_method"
+#    param + 1
+# end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def halve (num1)
+    if num1.class != Integer
+        return nil
+    end
+
+    num1/2
+end
